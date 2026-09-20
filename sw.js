6415e8f5dda754f34d2aa0ac63e3cfc53934896b
elwan-chess <990.sezer@gmail.com>
2026-09-20 14:27:08 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'elwan-chess-v1';
+const urlsToCache = [
+  './ElwanChess.html',
+  './elwan_cover.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
